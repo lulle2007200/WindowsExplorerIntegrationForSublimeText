e905a310e53a399ebd402ce56bddf12c58e0f03b
Christoph Baumann <lulle2007200@gmail.com>
2022-10-11 22:45:13 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -4,33 +4,18 @@ from package_control import events
 
 package_name = 'Sublime Explorer Integration'
 
-def install_event():
-	print("install event")
-
-def upgrade_event():
-	print("upgrade event")
-
-def pre_upgrade_event():
-	print("pre upgrade event")
-
-def uninstall_event():
-	print("uninstall event")
-
-def plugin_loaded():
-	print("hello")
-
 def plugin_loaded():
 	print("hello")
 
 	if events.install(package_name):
-		install_event()
+		print('install')
 	elif events.post_upgrade(package_name):
-		upgrade_event()
+		print('upgrade')
 
 def plugin_unloaded():
 	print("bye")
 
 	if events.pre_upgrade(package_name):
-		pre_upgrade_event()
+		print('pre upgrade')
 	elif events.remove(package_name):
-		uninstall_event()
+		print('uninstall')
