0b9143f002a78d587c5e06111a5209e72b79601a
Christoph Baumann <lulle2007200@gmail.com>
2022-10-11 22:40:14 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,36 +1,8 @@
-import sys
-import logging
 import sublime
 import sublime_plugin
 
-package_name = 'Windows 11 Explorer Integration'
-
-def install_event():
-	print("install event")
-
-def upgrade_event():
-	print("upgrade event")
-
-def pre_upgrade_event():
-	print("pre upgrade event")
-
-def uninstall_event():
-	print("uninstall event")
-
-
 def plugin_loaded():
 	print("hello")
-	from package_control import events
-
-	if events.install(package_name):
-		install_event()
-	elif events.post_upgrade(package_name):
-		upgrade_event()
 
 def plugin_unloaded():
-	from package_control import events
-
-	if events.pre_upgrade(package_name):
-		pre_upgrade_event()
-	elif events.remove(package_name):
-		uninstall_event()
+	print("bye")
